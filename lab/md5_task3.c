c52b5bc73d3cc58e46fed3483eb65141eb2b1b74
Soapmaze <lee141567@naver.com>
2020-05-22 06:01:36 +0900

    Add files via upload

--- lab/md5_task3.c
+++ lab/md5_task3.c
@@ -4,7 +4,6 @@ unsigned char abc[200] = { 'A','A','A','A','A','A','A','A','A','A','A','A','A','
 int main()
 {
         int i;
-        int flag=1;
         for(i = 0; i < 200; i++)
         {
 			printf("%x", abc[i]);
